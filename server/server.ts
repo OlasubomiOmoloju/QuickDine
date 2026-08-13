4e770e0ac42370d8a9143c246715de39de5e7d68
OlasubomiOmoloju <subby.omoloju@gmail.com>
2026-08-13 23:51:46 +0100

    Fix Vercel deployment

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import express, { NextFunction, Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
 import authRouter from "./routes/authRoutes.js";
@@ -14,31 +14,37 @@ const app = express();
 await connectDB();
 
 // Middleware
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
-const port = process.env.PORT || 5000;
-
-app.get('/', (req: Request, res: Response) => {
-    res.send('Server is Live!');
+app.get("/", (req: Request, res: Response) => {
+    res.send("Server is Live!");
 });
-app.use("/api/auth", authRouter)
-app.use("/api/restaurants", restaurantRouter)
-app.use("/api/bookings", bookingRouter)
-app.use("/api/owner", ownerRouter)
-app.use("/api/admin", adminRouter)
 
+app.use("/api/auth", authRouter);
+app.use("/api/restaurants", restaurantRouter);
+app.use("/api/bookings", bookingRouter);
+app.use("/api/owner", ownerRouter);
+app.use("/api/admin", adminRouter);
 
+// Global error handler
+app.use(
+    (
+        err: Error,
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ) => {
+        console.error("Unhandled Error:", err);
 
-//GLOBAL error handler
-app.use((err: Error, req: Request, res: Response, next: NextFunction)=>{
-    console.error("Unhandle Error:", err);
-    res.status(500).json({
-        message: err.message || "Internal Server Error",
-        stack: process.env.NODE_ENV === "production" ? undefined : err.stack,
-    });
-})
+        res.status(500).json({
+            message: err.message || "Internal Server Error",
+            stack:
+                process.env.NODE_ENV === "production"
+                    ? undefined
+                    : err.stack,
+        });
+    }
+);
 
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
-});
+export default app;
